bc34af52a0c12abec101e2b17fc0174a26ca1f19
falaprodavi <agencianatural@gmail.com>
2024-01-22 18:22:08 -0300

    :sparkles: feat: Pagina de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.logar, name="login"),
     path('cadastro/', views.cadastro, name="cadastro"),
     path('login/', views.logar, name="login"),
     path('logout/', views.logout, name="logout"),
